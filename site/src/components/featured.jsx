40ed26b6e1d716c56bc3d49a4d6aa082761e6c87
AdamSW1 <adamwintercs@gmail.com>
2025-01-22 15:04:32 -0500

    add header for featured projects section

---
+++ site/src/components/featured.jsx
@@ -0,0 +1,10 @@
+import './featured.css';
+export function Featured(){
+  return(
+    <div id="featuredSection" className="contentBlock">
+      <section>
+      <h1>Featured Projects</h1>
+      </section>
+    </div>
+  )
+}
--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,23 +1,29 @@
 import './Home.css';
+import {Featured} from '../components/featured'
 
 export function Home(){
   return(
-    <div id="home">
-      <h1>Hello!</h1>
-      <p id="aboutme">
-        My name is Adam Winter! I&apos;m a 19 year old computer science student currently enrolled 
-        at Dawson College!
-        <br/>
-        I&apos;m currently part of Dawson&apos;s robotics club and am captain of it&apos;s website team. <br />
-        As a team captain I&apos;m in charge of teaching members the basics of web development using react js and 
-        ensuring everyone has a role and work to do!
-      </p>
-      <div id="skills">
-      <h2>Skills</h2>
-      <p>Languages: Java, C#, python, Javascript, SQL, PL/SQL, Kotlin</p>
-      <p>Frameworks: React, Flask, Avalonia</p>
-      <p>Tools: Git, Visual Studio, Android Studio, Oracle SQL Developer </p>
-      </div>
+    <>
+    <div className="contentBlock">
+      <section>
+        <h1>Hello!</h1>
+        <p id="aboutme">
+          My name is Adam Winter! I&apos;m a 19 year old computer science student currently enrolled 
+          at Dawson College!
+          <br/>
+          I&apos;m currently part of Dawson&apos;s robotics club and am captain of it&apos;s website team. <br />
+          As a team captain I&apos;m in charge of teaching members the basics of web development using react js and 
+          ensuring everyone has a role and work to do!
+        </p>
+        <div id="skills">
+        <h2>Skills</h2>
+        <p>Languages: Java, C#, python, Javascript, SQL, PL/SQL, Kotlin</p>
+        <p>Frameworks: React, Flask, Avalonia</p>
+        <p>Tools: Git, Visual Studio, Android Studio, Oracle SQL Developer </p>
+        </div>
+      </section>
     </div>
+    <Featured/>
+    </>
   );
 }
