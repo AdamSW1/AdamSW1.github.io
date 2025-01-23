002053e80bb2e3cac3f1ad96ec0160c6648753bc
AdamSW1 <adamwintercs@gmail.com>
2025-01-23 11:08:21 -0500

    add image of me

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -7,7 +7,8 @@ export function Home(){
     <div className="contentBlock">
       <section className="contentBody">
         <h1>Hello!</h1>
-        <p id="aboutme">
+        <div id="fold">
+        <p id="aboutMe">
           My name is Adam Winter! I&apos;m a 19 year old computer science student currently enrolled 
           at Dawson College!
           <br/>
@@ -15,6 +16,10 @@ export function Home(){
           As a team captain I&apos;m in charge of teaching members the basics of web development using react js and 
           ensuring everyone has a role and work to do!
         </p>
+        <div id="myPhoto">
+            <img src="./images/me.png" alt="A picture of me"/>
+        </div>
+        </div>
         <div id="skills">
         <h2>Skills</h2>
         <p>Languages: Java, C#, python, Javascript, SQL, PL/SQL, Kotlin</p>
