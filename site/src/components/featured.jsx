29d6a5620cb361b189dc3714e7c0b187177ed33e
Adam Winter <adamseanwinter@outlook.com>
2025-01-23 02:19:56 -0500

    finish styling and layout for featured projects

--- site/src/components/featured.jsx
+++ site/src/components/featured.jsx
@@ -1,15 +1,21 @@
 import './featured.css';
-import {Project} from '../components/project';
-export function Featured(){
-  return(
+import { Project } from '../components/project';
+export function Featured() {
+  return (
     <div id="featuredSection" className="contentBlock">
       <section className="contentBody">
-      <h1>Featured Projects</h1>
-        <Project 
-          url="http://dawtrek.crcrobotics.com/"
-          imageUrl="./images/projects/dawtrek.png"
-          name="Dawson Robotics"
-        />
+        <h1>Featured Projects</h1>
+        <aside id="featuredProjects">
+          <Project
+            url="http://dawtrek.crcrobotics.com/"
+            imageUrl="./images/projects/dawtrek.png"
+            name="DawTrek Website"
+            type="Club Project"
+            year="2024"
+            description="The 2024 website for the CRC robotics website themed around LCARS devices in Star Trek."
+            languages={["NODE.JS", "REACT", "CSS"]}
+          />
+        </aside>
       </section>
     </div>
   )
--- site/src/components/project.jsx
+++ site/src/components/project.jsx
@@ -1,11 +1,27 @@
+/* eslint-disable react/prop-types */
 import './project.css'
-export function Project({url, imageUrl, name, type, year, description, languages }){
-  return(
+export function Project({ url, imageUrl, name, type, year, description, languages }) {
+  return (
     <section className="projectCard">
-      <a href={url}>
-        <img src={imageUrl} alt={name}/>
+      <a href={url} target="_blank">
+        <section className="projectImage">
+          <img src={imageUrl} alt={name} />
+        </section>
         <section className="projectInfo">
-        <p>{name}</p>
+          <h3 className="projectDetails">{year}, {type}</h3>
+          <h2 className="projectName">{name}</h2>
+          <section className="projectLanguages">
+            {
+              languages.map( lang =>{
+                return(
+                  <h4 key="lang">{lang}</h4>
+                )
+              })
+            }
+          </section>
+          <p>
+            {description}
+          </p>
         </section>
       </a>
     </section>
