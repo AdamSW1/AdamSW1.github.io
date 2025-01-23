128db305c24c1498a905ab2998371d3ea7ad2907
Adam Winter <adamseanwinter@outlook.com>
2025-01-23 02:37:06 -0500

    add competition of services project

--- site/src/components/featured.jsx
+++ site/src/components/featured.jsx
@@ -7,12 +7,23 @@ export function Featured() {
         <h1>Featured Projects</h1>
         <aside id="featuredProjects">
           <Project
+            url="http://15.222.62.124"
+            imageUrl="./images/projects/LDD.png"
+            name="A competition of services"
+            type="Academic Project"
+            year="2024"
+            description="A react js website comparing tv shows between cable television 
+                         providers and streaming services. Deployed using AWS"
+            languages={["NODE.JS", "REACT", "CSS"]}
+          />
+          <Project
             url="http://dawtrek.crcrobotics.com/"
             imageUrl="./images/projects/dawtrek.png"
             name="DawTrek Website"
             type="Club Project"
             year="2024"
-            description="The 2024 website for the CRC robotics website themed around LCARS devices in Star Trek."
+            description="The 2024 website for the CRC robotics website 
+                         themed around LCARS devices in Star Trek."
             languages={["NODE.JS", "REACT", "CSS"]}
           />
           <Project
@@ -21,7 +32,8 @@ export function Featured() {
             name="Solitaire in Java"
             type="Academic Project"
             year="2023"
-            description="A java console application for playing Solitaire. Made leveraging the concepts of inheritance and OOP principles"
+            description="A java console application for playing Solitaire. 
+                         Made leveraging the concepts of inheritance and OOP principles"
             languages={["Java"]}
           />
         </aside>
